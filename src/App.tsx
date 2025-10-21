da335bea9931b2bf70bdac32a4def1ebb158d4ea
venj0 <siradeghyansmbat@gmail.com>
2025-10-22 00:43:03 +0200

    fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { LoadingProvider } from "@/contexts/loading";
 import { AdminProvider }   from "@/contexts/admin";
 import "./main.css"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <LoadingProvider>
       <AdminProvider/>
+      <Analytics />
     </LoadingProvider>
   )
 }
